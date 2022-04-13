dbd844304c435ddf7c9d147dcc2eceea3c17ee44
Felipe Avelino <avelino.felipe@gmail.com>
2022-04-13 00:34:10 -0400

    quase pronto

--- AgrawaRicart.java
+++ AgrawaRicart.java
@@ -1,13 +1,12 @@
 import java.util.*;
 import java.net.*;
 import java.io.*;
-
+import java.time.*;
+import java.time.format.*;
 
 
 public class AgrawaRicart{
 	public static List<Process> cs_list;
-
-	
 
 	/*
 	Estados dos processos: 
@@ -20,18 +19,21 @@ public class AgrawaRicart{
 	static int estado = 1;
 	static String id;
 	static int porta;
-	static int timestamp;
-	static int votes = 0;
+	static int timestamp;//2.147.483.647
+	static int replies = 0;
+	static Object lock = new Object();
 	
 	public static void main(String args[]){ 
-		//um terminal: java Ric.java A0teste0 228.5.6.7
+		
+		//um terminal: java Ric.java A0 228.5.6.7
 		//outro terminal: java Ric.java A0escute0 228.5.6.7
 		// args give message contents and destination multicast group (e.g. "228.5.6.7")
 		//224.0.0.0 a 239.255.255.255
 
 		//int[] ports = {8111,8222,8333,8444};
-		estado = 0;		
+		
 		id = (args[0].split("0"))[0];
+
 		if (id.equals("A")) {
 			porta = 8111;
 		} else if (id.equals("B")) {
@@ -39,38 +41,103 @@ public class AgrawaRicart{
 		} else if (id.equals("C")) {
 			porta = 8333;
 		}
-
-		System.out.println("id: " + id);
 		
 		cs_list = new ArrayList<>();
 		
 		MulticastThread multicastThread = new MulticastThread(args[0], args[1]);
 		TCPServerThread tcpserverThread = new TCPServerThread();
-		//TCPClientThread tcpclientThread = new TCPClientThread("destino","mensagem");
 
-	}		      	
+		System.out.println("id: " + id);
+		//requestCriticalSection();
+
+		String opcao = null;
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Atividade 1 - Sockets");
+		System.out.println("Camila Puchta e Felipe Avelino");
+		
+		System.out.println("===================================");
+		System.out.println("Opções:");
+
+		while(true) {
+			while(true){
+				System.out.println("1: Entrar da seção critica");
+				opcao = scan.nextLine();
+				if (opcao.equals("1")) {
+					break;
+				}
+				System.out.println("Opcao invalida");
+			}
+			requestCriticalSection();
+			while(true){
+				System.out.println("1: Sair da seção critica");
+				opcao = scan.nextLine();
+				if (opcao.equals("1")) {
+					break;
+				}
+				System.out.println("Opcao invalida");
+			}
+			exitCriticalSection();
+		}
+		
+
+
+	}		   
+	
+	
 	
 	//chamo essa funcao pelo menu
 	public static void requestCriticalSection(){
 		//publica no multicast o pedido 
 		
 		/*
-		byte [] m = (AgrawaRicart.id + "0teste0").getBytes();
+		
+		
+		*/
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HHmmssSSS");
+
+		//criando um timestamp
+		AgrawaRicart.timestamp = Integer.parseInt(dtf.format(LocalTime.now()));
+		//Random rand = new Random();
+		//AgrawaRicart.timestamp = rand.nextInt(20000);
+		try {
+			InetAddress group = InetAddress.getByName("228.5.6.7");
+			MulticastSocket s = new MulticastSocket(6789);
+			s.joinGroup(group);
+			//byte [] m = ("A0teste0").getBytes();//exemplo de mensagem
+			byte [] m = (AgrawaRicart.id+"0request0"+AgrawaRicart.timestamp+"0").getBytes();
 			
 			//mensagem que eu vou enviar 
 			DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
 
 			//aqui que ele envia a mensagem pro grupo 
-			s.send(messageOut);	*/
-		
+			s.send(messageOut);	
+		} catch(Exception e ) {
+			System.out.println(e);
+		}
+
 		//muda o estado 
 		AgrawaRicart.estado = 2;//Wanted!
 		
 		//espera receber todos os OKs
-		
-		while (votes < 2) {
-				
+		/*
+		while (true) {
+			System.out.println(AgrawaRicart.replies);
+				if (AgrawaRicart.replies == 2){
+					break;
+				}
+		}*/
+		try {
+			synchronized (lock) {
+				lock.wait();
+			}
+			System.out.println("hello");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
+
+		System.out.println("Entrando na secao critica");
 
 		//muda o estado 
 		AgrawaRicart.estado = 3;//Held!
@@ -78,16 +145,7 @@ public class AgrawaRicart{
 		//entra na SC 
 		criticalSection();
 		
-		//muda o estado 
-		AgrawaRicart.estado = 1;//Released!
-
-		//sai da SC 
 		
-		//se a lista de processos for nao vazia, ele manda mensagem TCP pro proximo processo 
-		
-		//TCPClientThread tcpclientThread = new TCPClientThread(comandos[0],AgrawaRicart.id + "0OK0");
-		
-		votes = 0;
 	}
 
 	public static void criticalSection() {
@@ -97,49 +155,40 @@ public class AgrawaRicart{
 		//um jeito de dar sleep
 		try { Thread.sleep (2000); } catch (InterruptedException ex) {}
 	}
-	
-}
 
-public class Process {
-	String id;
-	int timestamp;
-	Process(String id, int timestamp){
-		this.id = id;
-		this.timestamp = timestamp;
+	//sai da SC 
+	public static void exitCriticalSection() {
+		//muda o estado 
+		AgrawaRicart.estado = 1;//Released!
+		AgrawaRicart.replies = 0;
+		
+		
+		//se a lista de processos for nao vazia, ele manda mensagem TCP pro proximo processo 
+		//TCPClientThread tcpclientThread = new TCPClientThread(comandos[0],AgrawaRicart.id + "0OK0");
+		if(!cs_list.isEmpty()){
+			Process menorTimestamp = new Process("menor", 1000000000);
+
+			for (Process it : cs_list) {           
+				if(it.timestamp <= menorTimestamp.timestamp){
+					menorTimestamp = it;
+				}    
+			}
+
+       		
+			   
+			System.out.println("Removeu da lsita: "+menorTimestamp.id);
+			TCPClientThread tcpclientThread = new TCPClientThread(menorTimestamp.id,AgrawaRicart.id+"0OK0");
+			cs_list.remove(menorTimestamp);
+        }
+		
+		
+
+		
 	}
 }
 
 
-/*
 
-Tipo1 objeto = new Tipo2();
-public class MinhaThreadRunnable implemens Runnable {
-	private String nome;
-	private int valorAdormecida;
-
-	public MinhaThreadRunnable (String nome, int valorAdormecida) {
-		this.nome = nome;
-		this.valorAdomercida = valorAdomercida;
-		//nao precisa mais do start 
-		//start();
-	}
-
-	public void run(){
-		System.out.println(nome + "foi iniciada");
-		try {
-			//faz as coisas aqui 
-		} catch (InterruptedException e) {
-			System.out.println();	
-		}
-	}
-	
-}
-
-public static void main (String []args){
-	MinhaThread thread1 = new MinhaThread("Thread1",500);
-	MinhaThread thread2 = new MinhaThread("Thread2", 900);
-}
-*/
 
 final class MulticastThread implements Runnable {
 	MulticastSocket s = null;
@@ -169,7 +218,7 @@ final class MulticastThread implements Runnable {
 			InetAddress group = InetAddress.getByName(groupName);
 			s = new MulticastSocket(6789);
 			s.joinGroup(group);
-			byte [] m = comandoterminal.getBytes();
+			/*byte [] m = comandoterminal.getBytes();
 			
 			//mensagem que eu vou enviar 
 			DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
@@ -180,7 +229,7 @@ final class MulticastThread implements Runnable {
 			//tem que separar os comandos com um caractere que nao de problema
 			//no caso escolhido foi o 0
 			comandos =  comandoterminal.split("0");
-			id = comandos[0];
+			id = comandos[0];*/
 			
 			//fica escutando o canal multicast 
 			while(true){
@@ -190,18 +239,42 @@ final class MulticastThread implements Runnable {
 				comandos = mensagem.split("0");
 				//System.out.println("comando: " + comandos[1]);
 				
-				/* Se o meu estado eh Wanted ou Held, eu vou adicionar a uma lista os processos que querem entrar */
-				AgrawaRicart.cs_list.add(new Process("A", rand.nextInt(10000)));
+				
 
-				if (comandos[1].equals("teste")){
+				if (comandos[1].equals("request")){//chega uma requisicao
 					
-					if (!comandos[0].equals(id) ) {
-						//manda unicast pro alvo
-						//pra nao ser tipo B ouviu e vai responder pra B 
-						System.out.println(AgrawaRicart.id + " ouviu em multicast e vai responder em unicast(TCP) pra " + comandos[0]);
-						//TCPClientThread tcpclientThread = new TCPClientThread("destino","mensagem");
-						TCPClientThread tcpclientThread = new TCPClientThread(comandos[0],AgrawaRicart.id + "0OK0");
+					
+					if (!comandos[0].equals(id)) {//se o id da requisicao eh diferente do meu 
+						//System.out.println("replies: " + AgrawaRicart.estado);
+						if (AgrawaRicart.estado == 1) {//Released
+							//manda unicast pro alvo
+							//pra nao ser tipo B ouviu e vai responder pra B 
+							System.out.println(AgrawaRicart.id + " ouviu em multicast e vai responder em unicast(TCP) pra " + comandos[0]);
+							//TCPClientThread tcpclientThread = new TCPClientThread("destino","mensagem");
+							TCPClientThread tcpclientThread = new TCPClientThread(comandos[0],AgrawaRicart.id+"0OK0");
+							
+						} 
+
+						if (AgrawaRicart.estado == 2) {//wanted
+							if (Integer.parseInt(comandos[2]) < AgrawaRicart.timestamp) {
+								//responde imediatamente via TCP
+								TCPClientThread tcpclientThread = new TCPClientThread(comandos[0],AgrawaRicart.id+"0OK0");
+							
+							}else{
+								//bota na fila
+								/* Se o meu estado eh Wanted ou Held, eu vou adicionar a uma lista os processos que querem entrar */
+								AgrawaRicart.cs_list.add(new Process(comandos[0], Integer.parseInt(comandos[2])));
+							}
+
+						} 
+	
+						if (AgrawaRicart.estado == 3) {//held
+							//bota na fila 
+							AgrawaRicart.cs_list.add(new Process(comandos[0], Integer.parseInt(comandos[2])));
+						} 
 					} 
+
+					
 					
 				}
 				System.out.println("MultiCast Received:" + new String(messageIn.getData()));
@@ -255,7 +328,18 @@ class Connection extends Thread {
 
 			String data = in.readUTF();	                  // read a line of data from the stream
 			System.out.println("Mensagem recebida de um cliente TCP: " + data);
-			out.writeUTF(data);
+			String []comandos = data.split("0");
+			if(comandos[1].equals("OK") && !AgrawaRicart.id.equals(comandos[0])) {
+				AgrawaRicart.replies +=1;
+				System.out.println("numero de replies: " + AgrawaRicart.replies);
+			}
+			if(AgrawaRicart.replies == 2) {
+				synchronized (AgrawaRicart.lock) {
+                    AgrawaRicart.lock.notify();
+                }
+			}
+			out.writeUTF("teste");
+			//out.writeUTF(data);
 		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
 		} catch(IOException e) {System.out.println("readline:"+e.getMessage());
 		} finally{ try {clientSocket.close();}catch (IOException e){/*close failed*/}}
@@ -298,12 +382,21 @@ final class TCPClientThread implements Runnable {
 			out.writeUTF(mensagem);//(args[0]);      	// UTF is a string encoding see Sn. 4.4
 			String data = in.readUTF();	    // read a line of data from the stream
 			//System.out.println("Received: "+ data) ; 
-			System.out.println("servidor recebeu!");
+			//System.out.println("servidor recebeu!");
 		}catch (UnknownHostException e){System.out.println("Socket:"+e.getMessage());
 		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
 		}catch (IOException e){System.out.println("readline:"+e.getMessage());
 		}finally {if(s!=null) try {s.close();}catch (IOException e){System.out.println("close:"+e.getMessage());}}
 
+	}
+}
+
+public class Process {
+	String id;
+	int timestamp;
+	Process(String id, int timestamp){
+		this.id = id;
+		this.timestamp = timestamp;
 	}
 }
 /*
@@ -350,3 +443,39 @@ final class TCPServer implements Runnable {
 
 }
 */
+
+
+
+/*
+
+Tipo1 objeto = new Tipo2();
+public class MinhaThreadRunnable implemens Runnable {
+	private String nome;
+	private int valorAdormecida;
+
+	public MinhaThreadRunnable (String nome, int valorAdormecida) {
+		this.nome = nome;
+		this.valorAdomercida = valorAdomercida;
+		//nao precisa mais do start 
+		//start();
+	}
+
+	public void run(){
+		System.out.println(nome + "foi iniciada");
+		try {
+			//faz as coisas aqui 
+		} catch (InterruptedException e) {
+			System.out.println();	
+		}
+	}
+	
+}
+
+public static void main (String []args){
+	MinhaThread thread1 = new MinhaThread("Thread1",500);
+	MinhaThread thread2 = new MinhaThread("Thread2", 900);
+}
+*/
+
+//https://www.guj.com.br/t/funcao-sleep/33034
+//https://stackoverflow.com/questions/16758346/how-pause-and-then-resume-a-thread
